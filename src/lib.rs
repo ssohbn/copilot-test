eadae61cbf9dd6f04d19801b1eb095cca66e62e2
me <me>
2022-03-26 15:55:18 -0700

    this is cool

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+// find a number in the fibbonacci sequence
+fn fibonacci(n: u32) -> u32 {
+	match n {
+		0 => 0,
+		1 => 1,
+		_ => fibonacci(n-1) + fibonacci(n-2),
+	}
+}
+
+#[cfg(tests)]
+mod tests {
+	#[test]
+	fn test_fibbonacci() {
+		assert_eq!(fibonacci(0), 0);
+		assert_eq!(fibonacci(1), 1);
+		assert_eq!(fibonacci(2), 1);
+		assert_eq!(fibonacci(3), 2);
+		assert_eq!(fibonacci(4), 3);
+		assert_eq!(fibonacci(5), 5);
+		assert_eq!(fibonacci(6), 8);
+		assert_eq!(fibonacci(7), 13);
+		assert_eq!(fibonacci(8), 21);
+		assert_eq!(fibonacci(9), 34);
+		assert_eq!(fibonacci(10), 55);
+		assert_eq!(fibonacci(11), 89);
+	}
+}
